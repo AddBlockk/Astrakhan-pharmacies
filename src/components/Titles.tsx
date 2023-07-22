434604838b7146cc6070dce0e7c93c596296263a
Dmitry <09.09.2005@inbox.ru>
2023-07-22 09:08:41 +0400

    fix

--- src/components/Titles.tsx
+++ src/components/Titles.tsx
@@ -1,28 +1,17 @@
 "use client";
 
-import { sectionTitle } from "@/app/utils/motion";
 import { motion } from "framer-motion";
 
 export function AboutTitle() {
   return (
-    <motion.div
-      className="text-[#C7C7C7] items-center text-[14px]"
-      variants={sectionTitle}
-      initial="hidden"
-      whileInView="show"
-    >
+    <motion.div className="text-[#C7C7C7] items-center text-[14px]">
       &#448; О Астраханских аптеках
     </motion.div>
   );
 }
 export function HistoryTitle() {
   return (
-    <motion.div
-      className="text-[#C7C7C7] items-center text-[14px]"
-      variants={sectionTitle}
-      initial="hidden"
-      whileInView="show"
-    >
+    <motion.div className="text-[#C7C7C7] items-center text-[14px]">
       &#448; История аптек
     </motion.div>
   );
